f1d1b57242a075efc0e9dfa7adf125e823f8a8a9
elbaz.ouad12492005 <elbaz.ouad1249@gmail.com>
2025-09-16 13:51:17 +0100

    JWT

--- django/mysit/settings.py
+++ django/mysit/settings.py
@@ -38,9 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'polls',
-    
     'rest_framework',
     'corsheaders',
+    
 ]
 
 CORS_ALLOWED_ORIGINS = [
@@ -132,3 +132,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        # 'rest_framework.authentication.BasicAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
+}
